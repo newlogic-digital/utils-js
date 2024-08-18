552b7fa91cc17ac90eb3dd827d850281403f73fa
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2024-08-18 10:49:14 +0200

    feat: 1.x

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import dataset from './dataset.js'
+import dispatchEvent from './dispatchEvent.js'
+import fetchJson from './fetchJson.js'
+import getId from './getId.js'
+import getUrl from './getUrl.js'
 import importScript from './importScript.js'
 import importStyle from './importStyle.js'
 import replaceScript from './replaceScript.js'
 import replaceScripts from './replaceScripts.js'
-import fetchJson from './fetchJson.js'
-import getUrl from './getUrl.js'
-import getId from './getId.js'
 import replaceVars from './replaceVars.js'
-import dispatchEvent from './dispatchEvent.js'
-import toggleDarkMode from './toggleDarkMode.js'
 import { initActions, initControllers } from './stimulus.js'
-import { NajaNewlogicDigitalExtension, initNaja, NajaFormValidityExtension, NajaRecaptchaExtension } from './naja.js'
-import { SwupNewlogicDigitalPlugin } from './swup.js'
+import toggleDarkMode from './toggleDarkMode.js'
 
 export {
     dataset,
@@ -19,10 +17,6 @@ export {
     importStyle,
     initActions,
     initControllers,
-    NajaNewlogicDigitalExtension,
-    NajaFormValidityExtension,
-    NajaRecaptchaExtension,
-    initNaja,
     replaceScript,
     replaceScripts,
     replaceVars,
@@ -30,6 +24,5 @@ export {
     getUrl,
     getId,
     dispatchEvent,
-    toggleDarkMode,
-    SwupNewlogicDigitalPlugin
+    toggleDarkMode
 }
