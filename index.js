b9437f685141757677b219043fb8785a1e9d852c
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2024-10-22 11:13:24 +0200

    feat: prod version

---
+++ index.js
@@ -0,0 +1,25 @@
+import dataset from './src/dataset.js'
+import dispatchEvent from './src/dispatchEvent.js'
+import fetchJson from './src/fetchJson.js'
+import getId from './src/getId.js'
+import getUrl from './src/getUrl.js'
+import importScript from './src/importScript.js'
+import importStyle from './src/importStyle.js'
+import replaceScript from './src/replaceScript.js'
+import replaceScripts from './src/replaceScripts.js'
+import replaceVars from './src/replaceVars.js'
+import toggleDarkMode from './src/toggleDarkMode.js'
+
+export {
+    dataset,
+    importScript,
+    importStyle,
+    replaceScript,
+    replaceScripts,
+    replaceVars,
+    fetchJson,
+    getUrl,
+    getId,
+    dispatchEvent,
+    toggleDarkMode
+}
